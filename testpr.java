1d73b225f38e50770fe20e6f760524a89c126af2
Kari <karisada41@gmail.com>
2018-08-10 15:24:18 -0500

    Agregar asserts de col

--- testpr.java
+++ testpr.java
@@ -1,6 +1,8 @@
 
-    public void open(int row, int col){
+public void open(int row, int col){
         assert row < n;
         assert row >= 0;
+        assert col < n;
+        assert col >= 0;
         grid[row][col] = true;
     }    // open site (row, col) if it is not open already
